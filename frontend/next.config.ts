2f2ca4ae6c456aa7f12c568a4ed347949e9cbae0
Arush <04arush@gmail.com>
2026-09-12 03:42:21 +0000

    Re-edit the configurations

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    return config;
-  },
+
 };
 
 export default nextConfig;
